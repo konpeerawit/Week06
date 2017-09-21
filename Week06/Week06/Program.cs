2dc804f5c5bff0172c482760eb9a4f7a0646fdbc
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-09-21 19:52:26 +0700

    Week06E02

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -11,22 +11,33 @@ namespace Week06
         static void Main(string[] args)
         {
             int n = 0;
-            ask(ref n);
-            answer(n);
-            Console.ReadKey();
+            int i = 0;
+            int[] numbers = new int[6];
+            ask (numbers[i]);
+            answer(numbers);
+
         }
-        static int ask(ref int n)
-        { 
-          Console.Write("Enter number : ");
-          n = int.Parse(Console.ReadLine());
-          return n;
-        }
-        static int answer(int n)
+        static int ask(int n, int[] numbers)
         {
-          Console.WriteLine("This number is : " + (n % 2 > 0 ? "Odd number" : "Even number"));
-          return n;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Enter numbers {0}", i);
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            return n;
         }
-            
+        static double answer(int n, int[] numbers)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                if ((numbers[i] % 7) = 0)
+                {
+                    Console.Write("");
+                }
+            }
+            return n;
+
+        }
     }
-    
 }
