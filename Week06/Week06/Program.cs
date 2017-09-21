c98dfa28db07670745b0616eb4ed4242b7961a19
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-09-21 19:58:45 +0700

    Week06P11

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -10,22 +10,13 @@ namespace Week06
     {
         static void Main()
         {
-            int x = 17;
-            double y = x / 4.0;
-            double z = y * y;
-            Console.WriteLine(y);
-            Console.WriteLine(z);
-
-            int a = 5;
-            int b = 4;
-            Console.WriteLine(a + b);
-            Console.WriteLine(a + (b++));
-            Console.WriteLine(a + b);
-            Console.WriteLine(a + (++b));
-            Console.WriteLine(a + b);
-            Console.WriteLine(14 / a);
-            Console.WriteLine(14 % a);
-
+            int x = 10, y = 5;
+            Console.WriteLine("x > y : " + (x > y));
+            Console.WriteLine("x < y : " + (x < y));
+            Console.WriteLine("x >= y : " + (x >= y));
+            Console.WriteLine("x <= y : " + (x <= y));
+            Console.WriteLine("x == y : " + (x == y));
+            Console.WriteLine("x != y : " + (x != y));
         }
     }
 }
