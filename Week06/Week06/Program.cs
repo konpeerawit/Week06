280a05ba10b6bc1b7b23b365cbbe2a0fa72a698c
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-09-21 23:47:33 +0700

    Week06E03

--- Week06/Week06/Program.cs
+++ Week06/Week06/Program.cs
@@ -11,19 +11,19 @@ namespace Week06
         static void Main(string[] args)
         {
             int n = 0;
-            int H, L1, L2;
+            int H, M1, M2;
             Console.Write("Enter height : ");
             H = int.Parse(Console.ReadLine());
             Console.Write("Enter long1 : ");
-            L1 = int.Parse(Console.ReadLine());
+            M1 = int.Parse(Console.ReadLine());
             Console.Write("Enter long2 : ");
-            L2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Trapezoid Area is : " + ans(H, L1, L2));
+            M2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Trapezoid Area is : " + ans(H, M1, M2));
             Console.ReadKey();
                     }
-        static double ans(int H, int L1, int L2)
+        static double ans(int H, int M1, int M2)
         {
-            return (0.5 * (L1 + L2) * H);
+            return (0.5 * (M1 + M2) * H);
         }
     }
 }
